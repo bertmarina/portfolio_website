354f37bc67a5a771d33f9acd0b6995f3dd5a6611
bertmarina <bertmarina@gmail.com>
2021-03-17 19:25:17 +0100

    Separated two different navs

--- php templates/nav_index.php
+++ php templates/nav_index.php
@@ -10,19 +10,19 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav ms-lg-auto ms-3">
         <li class="nav-item">
-          <a class="nav-link" href="../index.php">Home</a>
+          <a class="nav-link active" href="/index.php">Home <span class="sr-only">(current)</span></a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="../about/about.php">About</a>
+          <a class="nav-link" href="/about/about.php">About</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="../developer/developer.php">Developer</a>
+          <a class="nav-link" href="/developer/developer.php">Developer</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="../travel/travel.php">Travel</a>
+          <a class="nav-link" href="/travel/travel.php">Travel</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="../contact/contact.php">Contact</a>
+          <a class="nav-link" href="/contact/contact.php">Contact</a>
         </li>
       </ul>
     </div>
