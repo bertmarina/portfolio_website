102179e8f53fed74a97f166180e0ea645415a6d0
bertmarina <bertmarina@gmail.com>
2021-03-19 15:52:59 +0100

    Nav fixed

--- index.php
+++ index.php
@@ -17,7 +17,35 @@
 
 <body>
 
-  <?php include './php templates/nav.php' ?>
+<nav class="navbar navbar-expand-lg navbar-dark">
+    <a class="navbar-brand ms-md-3 ms-2" style="color: white;" href="/index.html">Marina B.</a>
+    <button class="navbar-toggler border-0 me-1 me-md-3" type="button" data-toggle="collapse"
+      data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+      aria-label="Toggle navigation">
+      <!-- <span class="navbar-toggler-icon"></span> -->
+      <i class="fas fa-bars"></i>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ms-lg-auto ms-3">
+        <li class="nav-item">
+          <a class="nav-link active" href="../index.php">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="../about/about.php">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="../developer/developer.php">Developer</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="../travel/travel.php">Travel</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="../contact/contact.php">Contact</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
 
   <main>
     <div class='content'>
