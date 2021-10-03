754cb08cd3a1b428066db52c56f74fb0fb8d69d6
bertmarina <bertmarina@gmail.com>
2021-10-03 21:42:09 +0200

    Added contact page and adjusted css

--- contact/contact.php
+++ contact/contact.php
@@ -3,30 +3,33 @@
 
 <head>
 
-<?php include '../php templates/head_links.php' ?>
+    <?php include '../php templates/head_links.php' ?>
 
-  <meta name="description" content="The list of countries I visited" />
-  <meta name="keywords" content="travel, map, world, marina, bertolini, marina bertolini, pictures" />
+    <meta name="description" content="Contact page" />
+    <meta name="keywords" content="travel, contact, email, marina, bertolini, marina bertolini" />
 
-  <link rel="stylesheet" type="text/css" href="../css/style.css" />
-  <!--collegamento a CSS-->
+    <link rel="stylesheet" type="text/css" href="../css/style.css" />
+    <!--collegamento a CSS-->
 
 </head>
 
 <body>
 
-<?php include '../php templates/nav.php' ?>
+    <?php include '../php templates/nav.php' ?>
 
-<main>
+    <main>
+        <div class="contact">
+            <h1 class="message">I'd <span class="heart">love</span> to hear from you!</h1>
+            <h5>You want to send me a postcard, a letter? Or do you want to simply have a chat? <br>
+                You can contact me through all the social media below or at my email <br>
+                <b>bertmarina</b> <i>at</i> <b>gmail</b> <i>dot</i> <b>com</b>.
+            </h5>
+        </div>
+    </main>
 
+    <?php include '../php templates/footer.php' ?>
 
-
-<div class="arrow"><a href="#top"><i class="fas fa-arrow-up"></i></a></div>
-</main>
-
-<?php include '../php templates/footer.php' ?>
-
-<?php include '../php templates/bootstrap_script.php' ?>
+    <?php include '../php templates/bootstrap_script.php' ?>
 
 </body>
 
